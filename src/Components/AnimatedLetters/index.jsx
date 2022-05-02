0bc4040b61c1b78e385de59a6516e8e7b17457a8
sahilrawat001 <sahilrawat80@gmail.com>
2022-05-02 07:17:53 +0530

    home section

---
+++ src/Components/AnimatedLetters/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.scss'
+const AnimatedLetters = () => {
+  return (
+<span>
+
+    
+</span>  )
+}
+
+export default AnimatedLetters
--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import "./index.scss"
-
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
 const Home = () => {
   return (
-    <div>Home</div>
+<div className="container home-page">
+ <div className="text-zone">
+<h1> Hi, <br/>
+I'm 
+ <img src={LogoTitle} alt="home logo"/>
+ahil <br/> Web Developer
+</h1>
+<h2>Frontend Developer</h2>
+<Link to='/contact' href="" className="flat-button" >
+Contact me
+</Link>
+ </div>
+ 
+</div>
+
   )
 }
 
