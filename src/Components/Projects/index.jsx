ec04a59300b760c628a2e898c570c0e8a661435f
sahilrawat001 <sahilrawat80@gmail.com>
2022-07-08 23:31:04 +0530

    project

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -3,18 +3,40 @@ import "./index.scss"
 const Project = () => {
   return (
     <div>
-         <div className="container home-page">
+         <div className="container project-page">
  <div className="text-zone">
  <h1>Projects</h1>
  <div className="list">
-  <li>
-  
+<div className="pro">
+<a target="_blank" rel="noreferrer" href=" https://weather-app-omega-teal.vercel.app/ " >
 
-  </li>
-    <h2> Weather App</h2>
-     
+    <h2 > Weather App</h2>
+    </a>
+    </div>   
+    <div className="pro">
+    <a target="_blank" rel="noreferrer" href="https://news-app-gamma-ten.vercel.app/" >
+
+     <h2>News App(Next.js) </h2>
+     </a>
+    </div>   
+    <div className="pro">
+    <a target="_blank" rel="noreferrer" href=" https://book-my-show-clone-orcin.vercel.app/ " >
+
     <h2> BookmyShow Clone</h2>
+    </a>
+    </div>   
+    <div className="pro">
+    <a target="_blank" rel="noreferrer" href="https://restraunt-website.vercel.app/ " >
+
+    <h2>Restraunt website</h2>
+    </a>
+    </div>   
+    <div className="pro">
+    <a target="_blank" rel="noreferrer" href=" https://zomato-webpage.vercel.app/ " >
+
     <h2> Zomato Page</h2>
+    </a>
+    </div>   
 
  </div>
  
